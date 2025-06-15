0f4be0ce8ad27be2c842147cd42cee1050612386
NAIM2046 <nmd98911@gmail.com>
2025-06-15 23:29:23 +0600

    frontend add hosting url

--- Frontend/src/TokenAdd/useAxiosPrivate.jsx
+++ Frontend/src/TokenAdd/useAxiosPrivate.jsx
@@ -1,23 +1,24 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
 
 const axiosPrivate = axios.create({
-    baseURL: "http://localhost:5000",
+  // baseURL: "http://localhost:5000",
+  baseURL: "https://projecteducation-91f3.onrender.com",
 });
 
 const useAxiosPrivate = () => {
-    axiosPrivate.interceptors.request.use(
-        (config) => {
-            const token = localStorage.getItem('token');
-            if (token) {
-                config.headers['Authorization'] = `Bearer ${token}`;
-            }
-            return config;
-        },
-        (error) => Promise.reject(error)
-    );
+  axiosPrivate.interceptors.request.use(
+    (config) => {
+      const token = localStorage.getItem("token");
+      if (token) {
+        config.headers["Authorization"] = `Bearer ${token}`;
+      }
+      return config;
+    },
+    (error) => Promise.reject(error)
+  );
 
-    return axiosPrivate;
+  return axiosPrivate;
 };
 
 export default useAxiosPrivate;
--- Frontend/src/TokenAdd/useAxiosPublic.jsx
+++ Frontend/src/TokenAdd/useAxiosPublic.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import axios from 'axios';
-
+import React from "react";
+import axios from "axios";
 
 const axiosPublic = axios.create({
-    baseURL: "http://localhost:5000",
+  //baseURL: "http://localhost:5000",
+  baseURL: "https://projecteducation-91f3.onrender.com",
 });
 
 const useAxiosPublic = () => {
-    return axiosPublic
+  return axiosPublic;
 };
 
 export default useAxiosPublic;
